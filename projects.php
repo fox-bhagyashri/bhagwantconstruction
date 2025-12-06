60232900b96a8c9063b63df8571f8daefb507113
fox-anil <anil.patharde@foxaircomm.com>
2025-12-06 17:38:56 +0530

    test

--- projects.php
+++ projects.php
@@ -1,6 +1,6 @@
 <?php
 ini_set("log_errors", 1);
-ini_set("error_log", "error_log.log");
+ini_set("error_log", __DIR__ . "/error_log.log");
 ini_set("display_errors", 0);
 
 include_once 'conn.php';
